5c14ad5bc800df8bea4c858dc4381f2290c1f1a4
SquawkyToucan <ewbrowne743@gmail.com>
2018-10-06 14:25:13 -0700

    Finished CSS, only have to make submit a quote page

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 function generateQuote() {
   var quotePartOne = ["My code is structurally oppressing me ", "I'm going to commit first ", "I would argue for authoritarianism ", "We should listen to what the government says ", "GitHub is bullying me ", "I like communism, ", "We should wear Hamilton shirts ", "GitHub is structurally oppressing us "];
-  var quotePartTwo = ["because we should listen to what the government says.", "because it's structurally oppresing me.", "like the time I beat up on a Bishop's kid.", "so I can do whatever the government tells me to", "so I don't get errors and you do ha", "because it's beautiful.", "because I am communist.", "because I'm wearing a Hamilton shirt."];
+  var quotePartTwo = ["because we should listen to what the government says.", "because it's structurally oppresing me.", "like the time I beat up on a Bishop's kid.", "so I can do whatever the government tells me to.", "so I don't get errors and you do ha", "because it's beautiful.", "because I am communist.", "because I'm wearing a Hamilton shirt."];
   var finishedQuote = "";
-  finishedQuote = quotePartOne[randomIntFromInterval(0, 7)] + quotePartTwo[randomIntFromInterval(0, 7)];
-  alert(finishedQuote);
+  finishedQuote = quotePartOne[randomNumber(0, 7)] + quotePartTwo[randomNumber(0, 7)];
   return finishedQuote;
 }
 
-function randomIntFromInterval(min,max) {
+function randomNumber(min,max) {
     return Math.floor(Math.random()*(max-min+1)+min);
 }
